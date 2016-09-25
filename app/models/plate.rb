9a7280fed67dccb65f394534b5065ea4df4d09b1
Maris <maris2143@gmail.com>
2016-09-25 14:10:58 -0500

    voting system

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,5 +1,6 @@
 class Plate < ActiveRecord::Base
   has_many :ingredients, dependent: :destroy
+  has_many :votes, dependent: :destroy
   validates :title, :description, :producer, presence: true
   validates :description, length: { minimum: 10 }
   validates :title, uniqueness: { scope: :producer, message: 'should happen once per producer' }
